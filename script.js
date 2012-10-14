d3cca40e948ebd6b81141fb249705d20362d36c8
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-10-14 17:12:39 +0530

    Added the message box now.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Prefix for the localStorage settings.
 var PREFIX = "restrict_facebook_";
 
+// The overlay HTML template.
+var HTML = '\
+<div id="overlay">\
+  <div id="message">\
+    <h1>Dude, get some work done !</h1>\
+    <p>Boss, you need some serious attention</p>\
+  </div>\
+</div>';
+
 // Settings variable, abstracts the way settings are stored.
 var settings = {
     get: function(key) {
@@ -15,21 +24,23 @@ var settings = {
 var options = {
     delay: 15, // The delay before the overlay appears.
     strict: true, // If the user can close the dialog or not.
+    messages: [], // The set of messages that the user might see.
 }
 
 var overlay = {
     show: function() {
         $("#overlay").fadeIn('fast', 'swing');
-        $("body").addClass("blur");
+        $("._li").addClass("blur");
+        $(".uiGrid").addClass("blur");
     },
     hide: function () {
         $("#overlay").fadeOut("fast");
-        $("body").removeClass("blur");
+        $("._li").removeClass("blur");
     }
 };
 
 function init() {
-    $("body").append('<div id="overlay"></div>');
+    $("body").append(HTML);
     overlay.show();
 }
 
