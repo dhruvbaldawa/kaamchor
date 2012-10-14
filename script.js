bb17752cb9f96cee5455117bae605785ec39ee6a
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-10-14 15:41:14 +0530

    Modified CSS, JS and added overlay

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// Prefix for the localStorage settings.
+var PREFIX = "restrict_facebook_";
+
+// Settings variable, abstracts the way settings are stored.
+var settings = {
+    get: function(key) {
+        return JSON.parse(localStorage[PREFIX + key]);
+    },
+    set: function(key, value) {
+        localStorage[PREFIX + key] = JSON.stringify(value);
+    }
+};
+
+// Options for the extension.
+var options = {
+    delay: 15, // The delay before the overlay appears.
+    strict: true, // If the user can close the dialog or not.
+}
+
+var overlay = {
+    show: function() {
+        $("#overlay").fadeIn('fast', 'swing');
+        $("body").addClass("blur");
+    },
+    hide: function () {
+        $("#overlay").fadeOut("fast");
+        $("body").removeClass("blur");
+    }
+};
+
+function init() {
+    $("body").append('<div id="overlay"></div>');
+    overlay.show();
+}
+
 $(document).ready(function() {
-  alert("Hello World !");
+    init();
 });
